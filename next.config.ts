498233d17e7d60ce105886b7d5a59b7bded0e41f
notnick2 <varun024123@gmail.com>
2024-12-17 12:33:58 +0530

    resolve1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  webpack: (config: { externals: string[] }, isServer: never) => {
+    if (isServer) {
+      config.externals.push('pptxgenjs')
+    }
+    return config
+  }
+}
